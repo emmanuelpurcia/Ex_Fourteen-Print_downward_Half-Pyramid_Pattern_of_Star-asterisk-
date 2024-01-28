5eb38ad05c4dbf1dee53bffab164b3008a4cb5c2
emmanuelpurcia <emmanpurcia023@gmail.com>
2024-01-29 01:01:11 +0800

    exercise_fourteen

---
+++ exercise_fourteen program.py
@@ -0,0 +1,8 @@
+# Hello! This program prints a downward half-pyramid pattern of star (asterisk)
+
+# pseudocode
+
+for i in range(6, 0, -1):
+    for j in range(i, 0, -1):
+        print("*", end=' ')
+    print("")
